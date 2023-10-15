432dacb65b46dc6dc34711fab485c8e7116054c4
Skeletus <Samol_88@hotmail.com>
2023-10-15 18:55:28 -0500

    HU Bibliotecario RegistrarPrestamo
    cuando  el bibliotecario selecciona un libro disponible para préstamo, identifica al usuario que lo solicita y registra la fecha de préstamo.
    
    el sistema actualiza el estado del libro como "prestado" y registra los detalles del préstamo, incluyendo la fecha de vencimiento.

--- biblioteca_online/biblioteca_online/models.py
+++ biblioteca_online/biblioteca_online/models.py
@@ -5,6 +5,9 @@ class Libro(models.Model):
     autor = models.CharField(max_length=100)
     isbn = models.CharField(max_length=13, unique=True)
     disponible = models.BooleanField(default=True)
+    fecha_prestamo = models.DateTimeField(null=True, blank=True)
+    fecha_vencimiento = models.DateTimeField(null=True, blank=True)
+    prestado_a = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self):
         return self.titulo
--- biblioteca_online/biblioteca_online/views.py
+++ biblioteca_online/biblioteca_online/views.py
@@ -5,3 +5,38 @@ def home_login(request):
 
 def register(request):
     return render(request,'register.html')
+
+from django.shortcuts import render, redirect
+from .forms import LibroForm
+
+def agregar_libro(request):
+    if request.method == 'POST':
+        form = LibroForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('confirmacion_agregar_libro')  # Redirige a la página de confirmación
+    else:
+        form = LibroForm()
+    
+    return render(request, 'agregar_libro.html', {'form': form})
+
+def confirmacion_agregar_libro(request):
+    return render(request, 'confirmacion_agregar_libro.html')
+
+"@login_required"
+def registrar_prestamo(request, libro_id):
+    libro = Libro.objects.get(pk=libro_id)
+    
+    if libro.disponible:
+        libro.disponible = False
+        libro.fecha_prestamo = timezone.now()
+        libro.fecha_vencimiento = timezone.now() + timezone.timedelta(days=30)  # Ejemplo de 30 días de préstamo
+        libro.prestado_a = request.user
+        libro.save()
+        return redirect('confirmacion_prestamo')
+    else:
+        # El libro ya está prestado, maneja esta situación en tu plantilla
+        return render(request, 'libro_prestado.html', {'libro': libro})
+
+def confirmacion_prestamo(request):
+    return render(request, 'confirmacion_prestamo.html')
