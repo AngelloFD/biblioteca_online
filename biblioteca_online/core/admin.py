50b65d7428c6ea3e49d130e587e3d42b2474a300
Angello Falla <fallaangello@gmail.com>
2023-10-19 14:20:59 -0500

    Aplicaciones, cambios en .gitignore e implementaciones

--- biblioteca_online/core/admin.py
+++ biblioteca_online/core/admin.py
@@ -1,13 +1,3 @@
 from django.contrib import admin
-from usuario.models import Usuario, InformeUsuario
-from .models import Libro, Categoria, Ejemplares, Prestamo, ReclamoUsuario, Actividad
 
 # Register your models here.
-admin.site.register(Usuario)
-admin.site.register(InformeUsuario)
-admin.site.register(Libro)
-admin.site.register(Categoria)
-admin.site.register(Ejemplares)
-admin.site.register(Prestamo)
-admin.site.register(ReclamoUsuario)
-admin.site.register(Actividad)
