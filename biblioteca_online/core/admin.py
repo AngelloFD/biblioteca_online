fa2cfce70bb705d6b983f3812dea5031ec4b3577
Angello Falla <fallaangello@gmail.com>
2023-10-19 22:49:08 -0500

    Modelos añadidos y habilitados para admin

--- biblioteca_online/core/admin.py
+++ biblioteca_online/core/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from usuario.models import Usuario, Estado, InformeUsuario
+from .models import Libro, Categoria, Ejemplares, Prestamo, ReclamoUsuario, Actividad
 
 # Register your models here.
+admin.site.register(Usuario)
+admin.site.register(Estado)
+admin.site.register(InformeUsuario)
+admin.site.register(Libro)
+admin.site.register(Categoria)
+admin.site.register(Ejemplares)
+admin.site.register(Prestamo)
+admin.site.register(ReclamoUsuario)
+admin.site.register(Actividad)
